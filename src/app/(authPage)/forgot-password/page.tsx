cf0c10b05931696c7baaaf81706a9bff9d6c86c1
thanghq24 <thang.hq@zinza.com.vn>
2023-05-25 15:07:38 +0700

    feat(ui): Implement Forgot password page
    Add Forgot password page
    Implement validation and fake API call

--- src/app/(authPage)/forgot-password/page.tsx
+++ src/app/(authPage)/forgot-password/page.tsx
@@ -1,7 +1,117 @@
-import React, { FC } from 'react';
+'use client';
+import {
+  Button,
+  FormControlLabel,
+  FormGroup,
+  Stack,
+  Switch,
+  Typography
+} from '@mui/material';
+import { red } from '@mui/material/colors';
+import TextInput from '@src/components/sharedComponents/TextInput';
+import React, { FC, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useRouter } from 'next/navigation';
+interface FormData {
+  email: string;
+}
+const schema = yup
+  .object()
+  .shape({
+    email: yup.string().email().required()
+  })
+  .required();
+
+const defaultValues = {
+  email: ''
+};
 
 const ForgotPassword: FC = () => {
-  return <div>ForgotPassword</div>;
+  const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isDirty, isValid }
+  } = useForm<FormData>({
+    mode: 'onChange',
+    defaultValues,
+    resolver: yupResolver(schema)
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(true);
+
+  const obSubmit = (data: FormData) => {
+    setLoading(true);
+    setTimeout(() => {
+      alert(
+        success
+          ? 'trying to log in with this data: ' +
+              JSON.stringify({ email: data.email })
+          : 'Có lỗi xảy ra'
+      );
+      setLoading(false);
+      success ? router.push('/login') : null;
+    }, 2000);
+  };
+
+  const canSubmit = !isValid || !isDirty || loading;
+
+  const goToLogin = () => {
+    router.push('/login');
+  };
+
+  const toggleSuccess = () => {
+    setSuccess((prev) => !prev);
+  };
+
+  return (
+    <Stack spacing={3} component="form" onSubmit={handleSubmit(obSubmit)}>
+      <Typography px={5} align="center">
+        Để khôi phục mật khẩu, vui lòng nhập đúng email bạn đã dùng để đăng ký{' '}
+        <Typography component="span" display="inline" color={red[600]}>
+          (*)
+        </Typography>
+      </Typography>
+      <TextInput
+        control={control}
+        name="email"
+        placeholder="Email"
+        errorMessage="Email không được bỏ trống và phải đúng định dạng"
+      />
+      <Stack direction="row" spacing={2} justifyContent="center">
+        <Button
+          variant="outlined"
+          color="primary"
+          size="large"
+          onClick={goToLogin}>
+          Quay lại
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          disabled={canSubmit}
+          sx={{
+            padding: '6px 32px'
+          }}>
+          Gửi
+        </Button>
+      </Stack>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch defaultChecked value={success} onChange={toggleSuccess} />
+          }
+          label="Fake API Call Success?"
+        />
+      </FormGroup>
+    </Stack>
+  );
 };
 
 export default ForgotPassword;
--- src/utils/muiTheme.ts
+++ src/utils/muiTheme.ts
@@ -1,8 +1,42 @@
 import { createTheme } from '@mui/material';
+import { indigo } from '@mui/material/colors';
 
 const theme = createTheme({
   palette: {
+    primary: {
+      main: indigo[700]
+    },
     success: { main: '#66BB6A', contrastText: '#fff' }
+  },
+  typography: {
+    button: {
+      fontSize: '1rem',
+      fontWeight: 700
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontSize: '1rem',
+          padding: '6px 16px',
+          borderRadius: '8px',
+          borderBottomLeftRadius: 0
+        },
+        sizeLarge: {
+          padding: '8px 22px'
+        }
+      }
+    },
+    MuiButtonBase: {
+      styleOverrides: {
+        root: {
+          fontSize: '1rem',
+          borderRadius: '8px',
+          borderBottomLeftRadius: 0
+        }
+      }
+    }
   }
 });
 
