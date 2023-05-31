233f61c74d183a3cfca53ac4d10e1f2b4e4d4d48
thanghq24 <thang.hq@zinza.com.vn>
2023-05-31 15:10:41 +0700

    feat(ui):  implement page header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 'use client';
-import { Container, Stack } from '@mui/material';
-import DailyStatistics from '@src/components/homepage/DailyStatistics';
-import StatisticalData from '@src/components/homepage/StatisticalData';
-import VaccinationPoint from '@src/components/homepage/VaccinationPoint';
+import Header from '@src/components/sharedComponents/PageHeader/Header';
 import { FC } from 'react';
 
 const Home: FC = () => {
   return (
-    <Stack spacing={4} py={4}>
-      <StatisticalData />
-      <DailyStatistics />
-      <VaccinationPoint />
-    </Stack>
+    <main>
+      <Header />
+    </main>
   );
 };
 
