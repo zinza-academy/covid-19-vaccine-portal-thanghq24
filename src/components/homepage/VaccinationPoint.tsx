ef4f370528e38ca9fb63e03dfb56a6d175456c37
thanghq24 <thang.hq@zinza.com.vn>
2023-06-01 11:09:59 +0700

    feat(ui): add vaccination points table to homepage

--- src/components/homepage/VaccinationPoint.tsx
+++ src/components/homepage/VaccinationPoint.tsx
@@ -1,12 +1,15 @@
-import { Paper, Stack, Typography } from '@mui/material';
+import { Divider, Paper, Stack, Typography } from '@mui/material';
 import React, { FC } from 'react';
 import SearchSection from './SearchSection';
+import VaccinationPointTable from './VaccinationPointTable';
 
 const VaccinationPoint: FC = () => {
   return (
     <Paper elevation={6} sx={{ padding: '24px 16px' }}>
       <Typography variant="h6">Tra cứu điểm tiêm theo địa bàn</Typography>
       <SearchSection />
+      <Divider sx={{ pt: 2 }} />
+      <VaccinationPointTable />
     </Paper>
   );
 };
