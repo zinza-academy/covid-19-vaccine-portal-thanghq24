6bf44ba7b183cb74a15a7de1f05a1fe02f87ad65
thanghq24 <thang.hq@zinza.com.vn>
2023-06-02 13:39:04 +0700

    feat: Add step 1 of vaccination registration form

--- src/utils/ReduxStoreProvider.tsx
+++ src/utils/ReduxStoreProvider.tsx
@@ -3,9 +3,19 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@src/redux/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store);
 
 const ReduxStoreProvider: FC<PropsWithChildren> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default ReduxStoreProvider;
