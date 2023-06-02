5e5f07ed1d1cc9922ad691eba97c4d3620d2bb73
thanghq24 <thang.hq@zinza.com.vn>
2023-06-02 13:39:04 +0700

    feat: Add step 1 of vaccination registration form

---
+++ src/utils/constants/jobs.tsx
@@ -0,0 +1,14 @@
+const jobGroups = [
+  { value: 0, label: 'Nhóm ngành sản xuất và chế biến' },
+  { value: 1, label: 'Nhóm ngành kiến trúc và xây dựng' },
+  { value: 2, label: 'Nhóm những ngành kinh doanh' },
+  { value: 3, label: 'Nhóm ngành luật - nhân văn' },
+  { value: 4, label: 'Nhóm ngành nghệ thuật - thẩm mỹ - đồ họa' },
+  { value: 5, label: 'Nhóm ngành báo chí - khoa học và xã hội' },
+  { value: 6, label: 'Nhóm ngành khoa học cơ bản' },
+  { value: 7, label: 'Nhóm ngành Sư phạm' },
+  { value: 8, label: 'Nhóm ngành nông - lâm - ngư nghiệp' },
+  { value: 9, label: 'Khác' }
+];
+
+export default jobGroups;
---
+++ src/utils/constants/vaccinationRegisterNotes.ts
@@ -0,0 +1,9 @@
+const notes = [
+  `Việc đăng ký thông tin hoàn toàn bảo mật và phục vụ cho chiến dịch tiêm chủng Vắc xin COVID - 19.`,
+  `Xin vui lòng kiểm tra kỹ các thông tin bắt buộc(VD: Họ và tên, Ngày tháng năm sinh, Số điện thoại, Số CMND/CCCD/Mã định danh công dân/HC...).`,
+  `Bằng việc nhấn nút "Xác nhận", bạn hoàn toàn hiểu và đồng ý chịu trách nhiệm với các thông tin đã cung cấp.`,
+  `Cá nhân/Tổ chức đăng ký thành công trên hệ thống sẽ được đưa vào danh sách đặt tiêm.`,
+  `Cơ sở y tế sẽ thông báo lịch tiêm khi có vắc xin và kế hoạch tiêm được phê duyệt. Trân trọng cảm ơn!`
+];
+
+export default notes;
