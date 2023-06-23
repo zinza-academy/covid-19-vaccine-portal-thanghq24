03373f29ac9dd1193fd16c0fed23df35d4a0b143
thanghq24 <thang.hq@zinza.com.vn>
2023-06-23 17:07:11 +0700

    fix: add response type to apis

--- src/api/authApi/resetPassword.tsx
+++ src/api/authApi/resetPassword.tsx
@@ -10,6 +10,15 @@ interface ResetPasswordResponseType {
   message: string;
 }
 
+interface VariablesType {
+  password: string;
+  token: string;
+}
+
+interface ResetPasswordResponseType {
+  message: string;
+}
+
 const resetPasswordApi = async (password: string, token: string) => {
   const { data } = await api.post<ResetPasswordResponseType>(
     'auth/change-password/' + token,
