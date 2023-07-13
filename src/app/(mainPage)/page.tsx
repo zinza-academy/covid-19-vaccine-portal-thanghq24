96991821e9899ad6c999c67091c0e43830aec2cf
thanghq24 <thang.hq@zinza.com.vn>
2023-07-13 14:32:02 +0700

    ci: test action runner 4

--- src/app/(mainPage)/page.tsx
+++ src/app/(mainPage)/page.tsx
@@ -8,7 +8,9 @@ import { FC } from 'react';
 const Home: FC = () => {
   return (
     <Stack spacing={4} py={4}>
-      <div>this has been add because github action trigger docker compose</div>
+      <div>
+        this has been add because github action trigger docker compose, again
+      </div>
       <StatisticalData />
       <DailyStatistics />
       <VaccinationPoint />
